528b08bd503b3a748ecdd834fe2c3f838af77525
Shraban Kumar Naik <shraban020@gmail.com>
2025-05-24 14:00:24 +0000

    2 new files

---
+++ Prabhu/2_list_of_datatypes.py
@@ -0,0 +1,41 @@
+"""
+List of datatypes
+"""
+
+"""
+Category-1 : IMMUTABLE types
+        After creating variable with some value then that value CAN'T be changed throught the program,
+        We can just access/read that value stored in that variable
+
+    1. Numbers:
+        -- We have option to store numbers like int, float
+    2. Strings:
+        -- We have option to store text data like name, email-id, word, sentence, paragraph
+        -- Automatically, index number will be assigned to each character
+    3. Tuple:
+        -- We have option store multiple values like list of employee names
+        -- We can keep DUPLICATE values
+        -- Automatically, index number will be assigned to each value
+    4. Frozenset:
+        -- We have option store multiple values like list of employee names
+        -- We can keep UNIQUE values
+        -- NO index for each value
+
+Category-2 : MUTABLE types
+        After creating variable with some value then that value CAN be changed throught the program,
+        We can ADD/REMOVE/UPDATE the value stored in that variable
+
+
+    1. List:
+        -- We have option store multiple values like list of employee names
+        -- We can keep DUPLICATE values
+        -- Automatically, index number will be assigned to each value
+    2. Set:
+        -- We have option store multiple values like list of employee names
+        -- We can keep UNIQUE values
+        -- NO index for each value
+    3. Dictionary:
+        -- We have option store multiple values like list of employee names
+        -- We can keep DUPLICATE values
+        -- Manually We-have-to/we-need-to provide index to each value(KEY/VALUE pair)
+"""
